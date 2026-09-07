950a01c08a89e9803377461b1c3558a22527125e
fatema-maitham <fatemamaitham1@gmail.com>
2026-09-07 09:49:45 +0300

    index amd show

--- src/services/petService.js
+++ src/services/petService.js
@@ -1,3 +1,18 @@
 // src/services/petService.js
 
 const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/pets`;
+
+const index = async () => {
+  try {
+    const res = await fetch(BASE_URL);
+    const data = await res.json();
+
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export {
+  index,
+};
